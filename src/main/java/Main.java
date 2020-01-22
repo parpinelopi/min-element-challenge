f54f50507b0f1c27587ab27ab5a8d1470aa8bafa
Penelope <pennyblack.p@gmail.com>
2020-01-22 15:26:53 +0100

    added findMin method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,45 +2,47 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Enter count");
         int count = scanner.nextInt();
         scanner.nextLine();
 
-        int[] returnedArray= readIntegers(count);
-
+        int[] returnedArray = readIntegers(count);
+        int returnedMin = findMin(returnedArray);
+        System.out.println("min =" + returnedMin);
 
 
     }
 
-    public static int[] readIntegers(int count){
-        int [] array = new int[count];
+    public static int[] readIntegers(int count) {
+        int[] array = new int[count];
 
-        for (int i=0; i<array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             System.out.println("insert number");
             int number = scanner.nextInt();
             scanner.nextInt();
-            array[i]=number;
+            array[i] = number;
         }
         return array;
     }
 
-    public static void findMin(int[] array){
-        int i;
-        int firstInt=0;
-        int minInt=0;
-        int nextInt=0;
-        for (i=0; i< array.length){
-            if(firstInt<nextInt){
-                firstInt = minInt;
+    public static int findMin(int[] array) {
 
-            }
-            if (minInt<nextInt){
+        int minInt = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            if (value < minInt) {
+                minInt = value;
 
             }
 
-            i++;
         }
+        return minInt;
+
     }
 
 }
+
+
